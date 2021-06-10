40b519009e2dce11b0f7874d0d789ce0612d80a0
jbyf <506921864@qq.com>
2021-06-10 19:30:19 +0800

    加上了pom和Application的注释

--- 001-springboot/src/main/java/com/jbyf/springboot/Application.java
+++ 001-springboot/src/main/java/com/jbyf/springboot/Application.java
@@ -3,7 +3,8 @@ package com.jbyf.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//SpringBoot项目启动入口类
+@SpringBootApplication//springboot 核心注解,主要用于开启spring自动配置
 public class Application {
 
     public static void main(String[] args) {
